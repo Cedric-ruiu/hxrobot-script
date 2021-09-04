d9a0b4439d77a244f76fa0fb361d82cbe8789f47
Cédric Ruiu <cedric.ruiu@gmail.com>
2021-09-04 11:09:06 +0200

    refactor: move version into static parameter

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import {Strategy} from './Strategy'
 
 const importStrategy = new Strategy();
 
-console.groupCollapsed('HXROBOT-SCRIPT v1.2.0');
+console.groupCollapsed(`HXROBOT-SCRIPT v${Strategy.version}`);
 console.log('%c// 1. choose the strategy panel', 'color:#ccc');
 console.log('let strat1 = new Strategy(0);');
 console.log('%c// 2. init with options', 'color:#ccc');
